255056431f7371f5c7946981c4b1735825a683f0
Lars Hagemann <hagemann.l@outlook.com>
2022-01-17 16:25:44 +0100

    Create <Particle> class

---
+++ inc/implementation/rendering/Particle.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "implementation/misc/Transform.hpp"
+
+
+namespace orbit
+{
+
+    class Particle
+    {
+    private:
+        // @member: position/rotation/scaling of this particle
+        Transform m_transform;
+    public:
+        
+    };
+
+}
